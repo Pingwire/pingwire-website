11a6d0ef3fdeb628fdeab176e04b8f06ce396d96
BrunoGodefroy <godefroy.bruno77@gmail.com>
2021-09-26 21:32:01 +0200

    Use new favicon and generate all the required sizes

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,5 +2,14 @@ module.exports = {
   plugins: [
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        name: 'Pingwire',
+        short_name: 'Pingwire',
+        start_url: '/',
+        icon: 'src/favicon.svg',
+      },
+    },
   ],
 };
