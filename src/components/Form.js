661704741521d80d6f3d5160ae7e322028c11c4c
Emil Hietanen <emil.hietanen@pingwire.io>
2023-05-12 07:43:50 +0200

    Use style.css instead of styled components for the form

--- src/components/Form.js
+++ src/components/Form.js
@@ -1,88 +1,18 @@
+/* eslint-disable react/no-unknown-property */
+/* eslint-disable jsx-a11y/label-has-associated-control */
 import { StaticImage } from 'gatsby-plugin-image';
 import React from 'react';
-import styled from 'styled-components';
-
-const Container = styled.div`
-  display: flex;
-  gap: 15px;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
-  min-height: 100vh;
-  background: #f9f9f9;
-  font-family: 'Roboto Mono';
-`;
-
-const StyledForm = styled.form`
-  display: flex;
-  flex-direction: column;
-  padding: 4%;
-  border-radius: 10px;
-  box-shadow: 0px 0px 30px rgba(0, 0, 0, 0.1);
-  background: rgba(33, 64, 50, 1);
-  color: white;
-  width: 95%;
-  max-width: 400px;
-
-  @media (min-width: 768px) {
-    width: 50%;
-    padding: 2%;
-  }
-`;
-
-const StyledLabel = styled.label`
-  font-size: 18px;
-  margin-bottom: 10px;
-  width: 100%
-`;
-
-const StyledInput = styled.input`
-  margin-bottom: 20px;
-  padding: 15px 0px;
-  border-radius: 5px;
-  border: 1px solid #ddd;
-  width: 100%;
-  font-size: 16px;
-  outline: none;
-`;
-
-const StyledTextArea = styled.textarea`
-  margin-bottom: 20px;
-  padding: 15px 0px;
-  border-radius: 5px;
-  border: 1px solid #ddd;
-  width: 100%;
-  height: 150px;
-  font-size: 16px;
-  resize: none;
-  outline: none;
-`;
-
-const StyledButton = styled.button`
-  padding: 15px 30px;
-  border: none;
-  border-radius: 5px;
-  background-color: #f9f9f9;
-  color: rgba(33, 64, 50, 1);
-  cursor: pointer;
-  transition: background-color 0.3s ease;
-  font-size: 16px;
-  width: 100%;
-
-  &:hover {
-    background-color: #ddd;
-  }
-`;
+import '../styles.css';
 
 const Form = () => (
-  <Container>
+  <div className="container">
     <StaticImage
       src="../images/Pingwire_logo.png"
       alt="Pingwire logo"
       placeholder="tracedSVG"
       layout="constrained"
     />
-    <StyledForm name="contact" method="post" netlify>
+    <form className="styled-form" name="contact" method="post" netlify data-netlify-recaptcha="true">
       <input type="hidden" name="form-name" value="contact" />
       <input
         type="hidden"
@@ -90,22 +20,23 @@ const Form = () => (
         value="New lead from %{formName} (%{submissionId})"
       />
       <p>
-        <StyledLabel>Your Name:</StyledLabel>
-        <StyledInput type="text" name="name" />
+        <label className="styled-label">Your Name:</label>
+        <input className="styled-input" type="text" name="name" />
       </p>
       <p>
-        <StyledLabel>Your Email:</StyledLabel>
-        <StyledInput type="email" name="email" />
+        <label className="styled-label">Your Email:</label>
+        <input className="styled-input" type="email" name="email" />
       </p>
       <p>
-        <StyledLabel>Message:</StyledLabel>
-        <StyledTextArea name="message" />
+        <label className="styled-label">Message:</label>
+        <textarea className="styled-textarea" name="message" />
       </p>
+      <div data-netlify-recaptcha="true" />
       <p>
-        <StyledButton type="submit">Send</StyledButton>
+        <button className="styled-button" type="submit">Send</button>
       </p>
-    </StyledForm>
-  </Container>
+    </form>
+  </div>
 );
 
 export default Form;
