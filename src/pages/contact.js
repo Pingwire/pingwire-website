702a90b5db7c5ec3350319209a5f540ee0ed4f25
Emil Hietanen <emil.hietanen@pingwire.io>
2023-05-12 11:16:49 +0200

    Use new page props

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 import Form from '../components/Form';
+import Page from '../components/Page';
 
 const Contact = () => (
-  <Form />
+  <Page title="Contact us | Pingwire" metaDescription="Contact us" footer={false} fullWidth>
+    <Form />
+  </Page>
 );
 
 export default Contact;
